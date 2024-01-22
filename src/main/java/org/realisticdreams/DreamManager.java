9b29776813e9b0df6e58b5ddf2f5d23a5665b363
Berkanktk <berkank@live.dk>
2024-01-22 16:17:39 +0100

    Minor cleanup

--- src/main/java/org/realisticdreams/DreamManager.java
+++ src/main/java/org/realisticdreams/DreamManager.java
@@ -9,7 +9,6 @@ import org.realisticdreams.utility.RandomText;
 import org.realisticdreams.utility.TF;
 import org.realisticdreams.quests.QuestAssignmentHandler;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
@@ -28,7 +27,6 @@ public class DreamManager {
     List<String> flyingQuotes = quotesLoader.getQuotes("flyingQuotes");
     List<String> nightmareQuotes = quotesLoader.getQuotes("nightmareQuotes");
 
-
     // Duration of potion effects is in ticks, 20 ticks = 1 second
     // https://hub.spigotmc.org/javadocs/bukkit/org/bukkit/potion/PotionEffectType.html
     public void applyDream(Player player) {
@@ -41,23 +39,21 @@ public class DreamManager {
                 break;
             case ADVENTUROUS:
                 player.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 1200, 1));
-                player.sendMessage(TF.format("You dreamt of grand adventures!", TF.GREEN));
+                player.sendMessage(TF.format("You dreamt of great adventures!", TF.GREEN));
                 player.sendMessage(randomText.getRandomText(adventurousQuotes, RandomText.DREAM.GOOD));
                 questAssignmentHandler.assignAdventureQuest(player);
                 break;
             case WEIRD:
-                player.sendMessage(TF.format("You have a bizarre and inexplicable dream!", TF.RED));
+                player.sendMessage(TF.format("You had an bizarre and inexplicable dream.", TF.RED));
                 player.sendMessage(randomText.getRandomText(weirdQuotes, RandomText.DREAM.BAD));
                 break;
             case NIGHTMARE:
-                player.setHealth(10);
-                player.sendMessage(TF.format("You wake up from a terrible nightmare!", TF.RED));
+                player.sendMessage(TF.format("You woke up from a terrible nightmare!", TF.RED));
                 player.sendMessage(randomText.getRandomText(nightmareQuotes, RandomText.DREAM.BAD));
                 break;
             case EXISTENTIAL:
                 player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 300, 1));
                 player.sendMessage(TF.format("You had an existential dream.", TF.GREEN));
-//                player.sendMessage(TF.format(existentialQuotes.get(random.nextInt(existentialQuotes.size())), TF.GREEN, false, true));
                 player.sendMessage(randomText.getRandomText(existentialQuotes, RandomText.DREAM.GOOD));
                 break;
             case ROMANCE:
@@ -72,7 +68,7 @@ public class DreamManager {
                 break;
             case SLEEP_DISORDER:
                 player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 1200, 1));
-                player.sendMessage(TF.format("You dreamt of not being able to sleep.", TF.RED));
+                player.sendMessage(TF.format("You were not able to sleep.", TF.RED));
                 player.sendMessage(randomText.getRandomText(sleepDisorderQuotes, RandomText.DREAM.BAD));
                 break;
             case FLYING:
